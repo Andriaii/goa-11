141e2a41e729e0c170d978fce044adbf6accfd91
Andriaii <159134102+Andriaii@users.noreply.github.com>
2024-10-06 13:19:39 +0400

    Add files via upload

---
+++ GOA/day 101/hw.js
@@ -0,0 +1,40 @@
+function person(name , age, city){
+    name = "andrew";
+    age = 12;
+    city = "kutaisi";
+    console.log(person)
+}
+const form = document.querySelector('form');
+
+const accounts = [];
+
+class Account {
+    constructor(firstname, lastname, email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    printDetails() {
+        console.log(`Firstname: ${this.firstname}`);
+        console.log(`Lastname: ${this.lastname}`);
+        console.log(`Email: ${this.email}`);
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const firstname = form.firstname.value;
+    const lastname = form.lastname.value;
+    const email = form.email.value;
+
+    const account = new Account(firstname, lastname, email);
+    account.printDetails();
+    accounts.push(account);
+})
+
+if(form = console.log(`Firstname: ${this.firstname}`));
+
+
+let firstname = "andria"
